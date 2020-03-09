1f8fd11e31df39b85d261e8af22288638d16b5b7
Arnawk <arnawk@gmail.com>
2020-03-09 17:00:14 +0530

    added create usre

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class UserController extends Controller
+{
+    public function createUser ( Request $request ) {
+        return User::create($request->all);
+    } 
+}
